9cb0d1bb4a27199d16ddb706bee04620e6769b31
oliver <omiguezalonso@gmail.com>
2025-09-09 13:01:38 +0200

    Primer Commit: Subiendo kotlin a github

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "1ºKotlin_prueba_subir_github"
+include(":app")
