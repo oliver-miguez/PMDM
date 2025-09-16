1cee5d6184077d26f88438725da0c7901244e980
oliver <omiguezalonso@gmail.com>
2025-09-16 12:32:07 +0200

    Clases kotlin

--- Clases Kotlin/src/example/myapp/Aquarium.kt
+++ Clases Kotlin/src/example/myapp/Aquarium.kt
@@ -1,14 +1,53 @@
 package example.myapp
 
-class Aquarium {
+import kotlin.math.tan
+
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+/*
     var width: Int = 20
     var height: Int = 40
     var length : Int = 100
+*/
+
+    //Los init se ejecutan por orden según aparezcan en el código
+    //Se pueden usar todos lo que quieras
+    init {
+        println("Inicializando aquarium")
+    }
+/*
+    init {
+      1 litro = 1000cm³
+        println("Volumen con el init : ${width * length * height / 1000} Litros")
+    }
+    */
+
+    /*
+    Se pueden crear varios constructores
+    Según la documentación de kotlin solo debe existir un constructor principal
+    Cada constructor secundario debe de llamar siempre al constructor original
+    En este caso lo llama con un  :this()
+    */
+    constructor(numberOfFish: Int):this(){
+        // 2000cm³ por pez + más un zona extra para no derramar el agua
+            val tank = numberOfFish * 2000 * 1.1
+        //calcular la altura necesaria
+        height = (tank / (length * width)).toInt()
+    }//end constructor secundario
+
+
+    var volumen: Int
+        //getter de volumen
+        get() = width * height * length / 1000  // 1000 cm³ = 1 l
+        //Value es el valor que nosotros le añadamos en la clase Kotlin
+        set(value){
+            height = (value * 1000) / (width * length)
+        }
 
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        println("Volume:$volumen LITROS")
     }
 
 
--- Clases Kotlin/src/example/myapp/Main.kt
+++ Clases Kotlin/src/example/myapp/Main.kt
@@ -1,11 +1,25 @@
 package example.myapp
 
 fun buildAquarium(){
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 2000
-    myAquarium.printSize()
+//    val myAquarium = Aquarium()
+//    myAquarium.printSize()
+//    myAquarium.height = 2000
+//    myAquarium.printSize()
+//
+//    val aquarium2 = Aquarium(height = 1200)
+//    aquarium2.printSize()
+//
+//    val aquarium3 = Aquarium(height = 378785, length = 60787878)
+//    aquarium3.printSize()
+//
+//    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+//    aquarium4.printSize()
+
+    val aquarium5 = Aquarium(numberOfFish = 29)
+    aquarium5.printSize()
+    println("Volumen con el constructor secundario: ${aquarium5.width * aquarium5.length * aquarium5.height / 1000} LITROS")
 }
+
 fun main() {
     buildAquarium()
 }
