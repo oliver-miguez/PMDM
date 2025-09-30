f7832fe270c02f83beca7bc502263dd75c070bc0
oliver <omiguezalonso@danielcastelao.org>
2025-09-30 11:39:04 +0200

    Refactorización

--- Maquina de cafe/MiMaquinaDeCafe/src/Main.kt
+++ Maquina de cafe/MiMaquinaDeCafe/src/Main.kt
@@ -1,3 +1,4 @@
+
 /**
  * Simulación de una máquina de café utilizando un autómata finito.
  * La máquina tiene varios estados y transiciones entre ellos basadas en las acciones del usuario.
@@ -8,8 +9,8 @@ fun main() {
 
     // Simular el funcionamiento de la máquina de café
     while (true) {
-        val currentState = StateMachine.getState()
-        currentState.onEnter(StateMachine)
+        val currentState = StateMachine.getState() // Obtener el estado actual
+        currentState.onEnter(StateMachine) // Ejecutar la acción al entrar en el estado actual, entonces se mueve al siguiente estado
 
         // Salir del bucle si se alcanza el estado Idle después de completar una acción
         if (currentState is MaquinaCafeEstados.Idle) {
@@ -17,4 +18,5 @@ fun main() {
             break
         }
     }
+
 }
